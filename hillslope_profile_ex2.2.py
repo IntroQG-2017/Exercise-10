04725e916009a2f4e0318f9199cb7ffa29705173
Dave Whipp <davewhipp@users.noreply.github.com>
2017-11-12 22:29:33 +0200

    Formatting update

--- hillslope_profile_ex2.2.py
+++ hillslope_profile_ex2.2.py
@@ -1,16 +1,21 @@
+#!/usr/bin/env python3
+"""Calculates a steady-state profile of a diffusive hillslope.
+
+Description:
+    This script calculates a steady-state profile of a diffusive hillslope
+    with rivers incising at the lateral margins of the hillslope. It also
+    loads a topographic profile for comparison with the model predictions.
+    
+    User-defined variables are listed at the top of the script
+    
+Usage:
+    ./hillslope_profile_ex2.2.py
+    
+Author:
+    FIRSTNAME LASTNAME - DD.MM.YYYY
 """
-hillslope_profile_ex2.2.py
 
-This script calculates a steady-state profile of a diffusive hillslope with
-rivers incising at the lateral margins of the hillslope. It also loads a
-topographic profile for comparison with the model predictions.
-
-User-defined variables are listed at the top of the script
-
-@author: XXX - dd.mm.yyyy
-"""
-
-# Import libraries
+# Import NumPy and Matplotlib modules
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -22,29 +27,27 @@ kappa = 10.0   # Soil/sediment/rock diffusivity [m^2/a]
 
 #--- END USER-DEFINED VARIABLES -------------------------------------------#
 
-#--- BEGIN HILLSLOPE CALCULATIONS -----------------------------------------#
-
 # Convert erosion rate from mm/a to m/a
 U = U / 1000.0
 
 # Read data file
-data = 
+data = np.loadtxt()
 
 # Separate data file into different variables
-data_x =
-data_h =
+data_x = 
+data_h = 
 
 # Define full width of hillslope
-x =
+x = np.linspace()
 
-# Define empty h-value array
+# Define empty array to store hillslope elevations (h)
 h = np.zeros(len(x))
 
 # Loop over all x-values and calculate hillslope elevation
 for
     h[i] = (U/(2.0*kappa)) * (L**2.0 - x[i]**2.0)
 
-# Increase elevation of predicted profile to match observed river elevations
+# Increase elevation of calculated profile to match observed river elevations
 
 
 # Plot results
