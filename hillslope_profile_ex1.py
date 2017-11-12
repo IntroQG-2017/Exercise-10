21c2754994ec8a68a7117e6285fefc1321ee8e04
Dave Whipp <davewhipp@users.noreply.github.com>
2017-11-12 22:26:31 +0200

    Formatting update

--- hillslope_profile_ex1.py
+++ hillslope_profile_ex1.py
@@ -1,19 +1,22 @@
+#!/usr/bin/env python3
+"""Calculates a steady-state profile of a diffusive hillslope.
+
+Description:
+    This script calculates a steady-state profile of a diffusive hillslope with
+    rivers incising at the lateral margins of the hillslope.
+    
+    User-defined variables are listed at the top of the script
+Usage:
+    ./hillslope_profile_ex1.py
+Author:
+    XXX YYY - DD.MM.YYYY
 """
-hillslope_profile_ex1.py
 
-This script calculates a steady-state profile of a diffusive hillslope with
-rivers incising at the lateral margins of the hillslope.
-
-User-defined variables are listed at the top of the script
-
-@author: XXX - dd.mm.yyyy
-"""
-
-# Import libraries
+# Import NumPy and Matplotlib modules
 import numpy as np
 import matplotlib.pyplot as plt
 
-#--- USER-DEFINED VARIABLES BELOW -----------------------------------------#
+#--- USER-DEFINED VARIABLES -----------------------------------------------#
 
 L = 10.0        # Half-width (ridge to valley) of hillslope [m]
 U = 0.2         # Hillslope uplift rate [mm/a]
@@ -21,15 +24,13 @@ kappa = 0.01    # Soil/sediment/rock diffusivity [m^2/a]
 
 #--- END USER-DEFINED VARIABLES -------------------------------------------#
 
-#--- BEGIN HILLSLOPE CALCULATIONS -----------------------------------------#
-
 # Convert erosion rate from mm/a to m/a
 U = U / 1000.0
 
 # Define range for full width of hillslope
-x =
+x = np.linspace()
 
-# Define empty h-value array
+# Define empty array to store elevation values (h)
 h = np.zeros(len(x))
 
 # Loop over all values of x and calculate hillslope elevation
@@ -37,7 +38,7 @@ for
     h[i] = (U/(2.0*kappa)) * (L**2.0 - x[i]**2.0)
 
 # Plot results
-plt.plot(x,h,'k-')
+plt.plot(x, h, 'k-')
 plt.xlabel('Distance from divide [m]')
 plt.ylabel('Elevation [m]')
 plt.title('Predicted hillslope geometry: Diffusion model')
